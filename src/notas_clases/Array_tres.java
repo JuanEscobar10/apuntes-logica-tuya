53a17cd278bea4f3a21185f30fd7e06dc18a1921
JuanEscobar10 <juanescobardavid2007@hotmail.com>
2024-10-03 21:37:03 -0500

    en este realizamos Array, Array_dos, Array_tres, Consicional, Funcion

--- src/notas_clases/Array_tres.java
+++ src/notas_clases/Array_tres.java
@@ -1,4 +1,28 @@
 package notas_clases;
 
+import java.util.Scanner;
+
 public class Array_tres {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String [] nombres = new String[4];
+
+        int i =0;
+        while (i < nombres.length){
+
+            System.out.println("Ingrese Nombre " + (i+1));
+
+            nombres[i] = sc.nextLine();
+            i++;
+        }for (i=0; i < nombres.length;i++){
+            System.out.println("el nombre es: " + nombres[i]);
+        }
+
+
+
+
+    }
 }
--- src/notas_clases/Condicional.java
+++ src/notas_clases/Condicional.java
@@ -1,4 +1,44 @@
 package notas_clases;
 
+import java.util.Scanner;
+
 public class Condicional {
+
+    static Scanner sc = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+
+        //if es una estructura condicional, valida que se cumple una condición y ejecuta si es true
+        //dependiendo de si es compuesto, entonces genera una respuesta en caso que sea falso
+
+        double balance = 170000;
+
+        System.out.println("Ingrese el valor de la compra");
+        double purchase = sc.nextDouble();
+
+        System.out.println("Ingrese Número de cuotas");
+        int cuotas = sc.nextInt();
+
+        int total = (int) (purchase / cuotas);
+
+
+
+
+
+
+        if (purchase > 0 && purchase <= balance){
+            System.out.println("Compra exitosa");
+            balance -= purchase;
+            System.out.println("Su saldo es " + balance);
+            System.out.println("Tus cuotas son de: " + total);
+
+
+
+        }else if (purchase <= 0) {
+            System.out.println("Por favor revisar saldo");
+        }else {
+            System.out.println("Que pena saldo Insuficiente");
+        }
+    }
 }
