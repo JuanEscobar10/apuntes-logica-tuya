33c2ac3004a1d5970451a05570d0d496d756e79c
JuanEscobar10 <juanescobardavid2007@hotmail.com>
2024-10-03 21:17:01 -0500

    se hizo formulario_de_registro, Hola_mundo, Login, Menu_navegacion, Tienda_alex (No la terminamos)

--- src/ejercicios/Hola_mundo.java
+++ src/ejercicios/Hola_mundo.java
@@ -1,4 +1,8 @@
 package ejercicios;
 
 public class Hola_mundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
 }
--- src/ejercicios/Login.java
+++ src/ejercicios/Login.java
@@ -1,4 +1,27 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Login {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String Usuario = "juanescobar@gmail.com";
+        int Key = 1234;
+        String namberUser = "Juan";
+
+        //Intereces
+        System.out.println("Ingrese su  nombre de Usuario");
+        String userInput = sc.next();
+
+        System.out.println("Ingrese su password");
+        int keyInput = sc.nextInt();
+
+        if (Usuario.equals(userInput) && Key == keyInput){
+            System.out.println("Bienvenido " + namberUser );
+        }else {
+            System.out.println("Valide Credenciales");
+        }
+    }
 }
